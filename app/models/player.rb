e33bb2e2b4d58fa456c186be689840516b76c9a9
Dave Jameson <davetrippinon@gmail.com>
2023-05-25 10:55:39 +0900

    almost done world creation page

--- app/models/player.rb
+++ app/models/player.rb
@@ -3,7 +3,7 @@ class Player < ApplicationRecord
 
   has_many :encounters
 
-  accepts_nested_attributes_for :players, allow_destroy: true
+  # accepts_nested_attributes_for :players, allow_destroy: true
 
   validates :player_name, presence: true
   validates :character_name, presence: true
