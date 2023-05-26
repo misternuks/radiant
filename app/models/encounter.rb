34583f03fccdb15ed9fe1a3ce035082b89590bb4
Connor Minto <caminto94@gmail.com>
2023-05-26 13:27:54 +0900

    Does not allow campaign with blank players or blank enemies to be added;

--- app/models/encounter.rb
+++ app/models/encounter.rb
@@ -3,7 +3,10 @@ class Encounter < ApplicationRecord
   has_many :players, through: :active_players
   has_many :enemies
 
-  accepts_nested_attributes_for :enemies, allow_destroy: true
+  accepts_nested_attributes_for :enemies,reject_if: :reject_empty, allow_destroy: true
   # validates :skill_type, presence: true
   # validates :success, presence: true
+  def reject_empty(attributes)
+    attributes['name'].blank?
+  end
 end
