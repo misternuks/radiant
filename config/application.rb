c22a806686233ab88eb827d2834c0e04979df2fd
Connor Minto <caminto94@gmail.com>
2023-05-29 15:37:23 +0900

    Stashing;

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,8 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
+
 module Radiant
   class Application < Rails::Application
     config.generators do |generate|
@@ -15,7 +17,6 @@ module Radiant
     end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
