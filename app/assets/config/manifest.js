d47c12edb7aa9bd629f7a067fae616c61d1704b3
Connor Minto <caminto94@gmail.com>
2023-05-29 18:10:52 +0900

    Javascript working for forms, loading spinners added and forms extend as you go

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,3 +1,5 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
 //= link_tree ../builds
+//= link campaign.js
+//= link encounter.js
---
+++ app/assets/javascripts/campaign.js
@@ -0,0 +1,53 @@
+
+
+
+// for loading spinner on campaign/new
+const campaignForm = document.querySelector('#new_campaign')
+const loadingSpinner = document.querySelector('.hidden-spinner')
+const spinnerArea = document.querySelector('.spinner-area')
+
+
+// for loading spinner on encounter/edit
+// const encounterForm = document.querySelector('.encounter-form')
+
+
+//for form on encounter/edit
+// const formTarget = document.querySelector('#encounter_target')
+// const formTargetLabel = document.querySelector('#encounter_target_label')
+// const formSuccessLabel = document.querySelector('#encounter_success_label')
+// const formSuccess = document.querySelector('#encounter_success')
+// const formCriticalLabel = document.querySelector('#encounter_critical_label')
+// const formCritical = document.querySelector('#encounter_criticality')
+// const formSkillType = document.querySelector('#encounter_skill_type')
+
+
+// console.log(encounterForm)
+// // submits for encounter forms
+// formSkillType.addEventListener("change", (event) => {
+//     formTarget.classList.add('form-visible')
+//     formTargetLabel.classList.add('form-visible')
+// });
+
+// formTarget.addEventListener("input", (event) => {
+//   formSuccessLabel.classList.add('form-visible')
+//   formSuccess.classList.add('form-visible')
+
+// });
+
+// formSuccess.addEventListener("input", (event) => {
+//   formCriticalLabel.classList.add('form-visible')
+//   formCritical.classList.add('form-visible')
+
+
+
+// loading screen submits
+
+campaignForm.addEventListener("submit", (event) => {
+  loadingSpinner.classList.add('active-spinner');
+  loadingSpinner.classList.remove('hidden-spinner');
+  spinnerArea.remove();
+});
+
+// encounterForm.addEventListener("click", (event) => {
+//   console.log('OKAY')
+// });
